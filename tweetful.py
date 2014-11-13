aa810385b0e699c832ec226dcbbc731faeaa3d3b
alicyrus <ali.chehrehsaz@gmail.com>
2014-11-12 20:36:44 -0800

    first commit

---
+++ tweetful.py
@@ -0,0 +1,16 @@
+import authorization
+
+import json
+import requests
+
+from urls import *
+
+def main():
+    """ Main function """
+    auth = authorization.authorize()
+
+    response = requests.get(TIMELINE_URL, auth=auth)
+    print json.dumps(response.json(), indent=4)
+    
+if __name__ == "__main__":
+    main()
