1a96645e548f8ab2b5db71a90aed60ce99b010d4
alicyrus <ali.chehrehsaz@gmail.com>
2014-11-13 14:09:42 -0800

    implements argparse for TIMELINE and FRIEND_LIST api features

--- tweetful.py
+++ tweetful.py
@@ -5,14 +5,45 @@ import requests
 
 from urls import *
 
+import argparse
+import sys
+
+
+def make_parser():
+	"""Constructs the command line parser"""
+	description = "gets the API feature to access"
+
+	parser = argparse.ArgumentParser(description=description)
+
+	subparsers = parser.add_subparsers(dest="API_feature", help="Available features")
+
+	#subparser for TIMELINE
+	timeline_parser = subparsers.add_parser("TIMELINE", help="return JSON for user timeline")
+
+	#subparser for FRIEND_LIST
+	friend_list_parser = subparsers.add_parser("FRIEND_LIST", help="return JSON for user's friend list")
+
+	return parser
+
+
 def main():
     """ Main function """
+
+    parser = make_parser()
+    arguments = parser.parse_args(sys.argv[1:])
+    #convert parsed arguments from Namespace to dictionatry
+    arguments = vars(arguments)
+    api_feature = arguments.pop("API_feature")
+
     auth = authorization.authorize()
 
-    # response = requests.get(TIMELINE_URL, auth=auth)
+    if api_feature == "TIMELINE":
+        response = requests.get(TIMELINE_URL, auth=auth)
+        print json.dumps(response.json(), indent=4)
 
-    response = requests.get(FRIEND_LIST_URL, auth=auth)
-    print json.dumps(response.json(), indent=4)
+    if api_feature == "FRIEND_LIST":
+        response = requests.get(FRIEND_LIST_URL, auth=auth)
+        print json.dumps(response.json(), indent=4)
 
 
 if __name__ == "__main__":
